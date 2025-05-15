56f3a3b879f983e117f9811844497f880e6c6477
Bernie Ramon <bernie_ramone@outlook.com>
2025-05-14 21:18:52 -0600

    add db context

---
+++ MicroserviceFull/PlatformService/Data/PrepDb.cs
@@ -0,0 +1,26 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
